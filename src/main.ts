64e8dce6163797e6ab3d04b926d1256fa6d2e1f4
AleksMaifet <aleksmaifet@gmail.com>
2023-09-02 23:50:42 +0300

    added env app port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import * as process from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
